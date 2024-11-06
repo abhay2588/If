315034fec2f3ff4e5dac9184e4b4bbbbda6f3ecb
abhay2588 <160823533+abhay2588@users.noreply.github.com>
2024-11-06 20:49:30 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+#EXTM3U 
+#KODIPROP:inputstream.adaptive.license_type=org.w3.clearkey
+#KODIPROP:inputstream.adaptive.license_key={"keys":[{"kty":"oct","k":"zYTtE2sMxx-KuM1NT2ouTA","kid":"A8Lgry-BWfnwzptdvIZfEA"}],"type":"temporary"}
+#EXTINF:-1 tvg-id="197" group-title="Sports", Astro Cricket
+https://iii-silk.vercel.app/2504/default.mpd?|drmScheme=clearkey&drmLicense=03c2e0af2f8159f9f0ce9b5dbc865f10:cd84ed136b0cc71f8ab8cd4d4f6a2e4c
