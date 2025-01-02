a451291409f8089079f22b28c4d462b3d544ce1b
abhay2588 <160823533+abhay2588@users.noreply.github.com>
2025-01-02 09:55:35 +0530

    index.php

--- index.php
+++ index.php
@@ -19,14 +19,11 @@ https://linearjitp-playback.astro.com.my/dash-wv/linear/5055/default_primary.mpd
 #EXTINF:-1 tvg-id="414.astro" tvg-logo="https://linear-poster.astro.com.my/prod/logo/HBO_Family.png" group-logo="https://astrogo.astro.com.my/staticFiles/images/icons/astroLogo.png" group-title="Astro",Hbo Family   
 https://linearjitp-playback.astro.com.my/dash-wv/linear/5053/default_primary.mpd
 
-#EXT-X-SESSION-KEY:METHOD=SAMPLE-AES,URI="skd://1f52934d28a25c51a5dd577a650dcece",KEYFORMAT="com.apple.streamingkeydelivery",KEYFORMATVERSIONS="1"
-#EXT-X-SESSION-KEY:METHOD=SAMPLE-AES,URI="data:text/plain;base64,AAAAOHBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAABgSEB9Sk00oolxRpd1XemUNzs5I88aJmwY=",KEYID=0X1f52934d28a25c51a5dd577a650dcece,KEYFORMAT="urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed",KEYFORMATVERSIONS="1"
 #EXTINF:-1 tvg-logo=" https://images.fubo.tv/channel-config-ui/station-logos/on-dark/willow_2023_primary-white.png" group-title="Cricket ",WILLOW CRICKET
 #KODIPROP:inputstream.adaptive.license_type=clearkey
 #KODIPROP:inputstream.adaptive.manifest_type=dash
-#KODIPROP:inputstream.adaptive.license_key=61d5f72a22d9e8b6a953f3516e5d4f7f:c81009990766a50feae298ab74940db2
-https://abgqckoaaaaaaaambcrovz6jv4i5x.a44e5017b2a24d109a79eef5850f6b97.emt.cf.ww.aiv-cdn.net/Content/HLS.cps/
-Live/channel(WILLHD-3291.dfw.1080)/index.m3u8
+#KODIPROP:inputstream.adaptive.license_key=59ed8dc9d4a48857ef8e5865919496cc:0d3680bd8b349deda8b7fdd1da71b7f8
+https://cors-proxy.cooks.fyi/https://dfwlive-v1-c1p3-sponsored.akamaized.net/Content/HLS.cps/Live/channel(WILLHD-3291.dfw.1080)/index.m3u8
 
 #EXTINF:-1 tvg-logo=" https://images.fubo.tv/channel-config-ui/station-logos/on-dark/willow_2023_primary-white_xtra.png " group-title="Cricket",WILLOW XTRA
 #KODIPROP:inputstream.adaptive.license_type=clearkey
